291187324d442dd75b74b583ef2d292ba612af72
Diwhdkgxgx <jlj1144247539@outlook.com>
2021-03-14 14:45:10 +0800

    天气预报第二问未分开

---
+++ chapter_01/2.2.2.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+// #include <string.h>
+#include <iostream>
+// #include <cstring>
+
+
+using namespace std;
+
+// B 1 E 2 W 3
+
+int i;
+int main()
+{
+    // char a[10][10];
+    // char b[10];
+    // cin >> a[0][0];
+    // cin >> b;
+    // char ddd[5] = "abc";
+    // strcpy(ddd, b);
+    // strcmp(b, ddd);
+    // // cout << a[0][0] << endl;
+    // // cout << a[0][1] << endl;
+    // cout << ddd << endl;
+
+    char a[10];
+    cin >> a;
+    if (a[0] == 'a')
+    {
+        cout << a[0] << endl;
+    }
+    
+    
+    return 0;
+}
